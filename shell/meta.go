42e2e3e8e4fd9c7ce7a545bace4f2eca6bb9a64c
Daan Gerits <daan.gerits@gmail.com>
2024-05-20 13:15:50 +0200

    added parameter support
    Signed-off-by: Daan Gerits <daan.gerits@gmail.com>

--- shell/meta.go
+++ shell/meta.go
@@ -1,6 +1,9 @@
 package shell
 
+import "encoding/json"
+
 type Option func(map[string]string)
+type ParamOption func(*Parameter)
 
 func NewMetadata(opts ...Option) Metadata {
   m := map[string]string{}
@@ -24,6 +27,23 @@ func WithDescription(desc string) Option {
   }
 }
 
+func WithParameters(param ...Parameter) Option {
+  return func(m map[string]string) {
+    ps, fnd := m["_nats.shell.parameters"]
+    if !fnd {
+      ps = "[]"
+    }
+
+    var params []Parameter
+    _ = json.Unmarshal([]byte(ps), &params)
+
+    params = append(params, param...)
+
+    b, _ := json.Marshal(params)
+    m["_nats.shell.parameters"] = string(b)
+  }
+}
+
 func WithEntries(m map[string]string) Option {
   return func(m map[string]string) {
     for k, v := range m {
@@ -40,4 +60,23 @@ func (m Metadata) Summary() string {
 
 func (m Metadata) Description() string {
   return m["_nats.shell.description"]
+}
+
+func WithParameterSummary(summary string) ParamOption {
+  return func(p *Parameter) {
+    p.Summary = summary
+  }
+}
+
+func NewParameter(name string, opts ...ParamOption) Parameter {
+  p := Parameter{Name: name}
+  for _, opt := range opts {
+    opt(&p)
+  }
+  return p
+}
+
+type Parameter struct {
+  Name    string `json:"name"`
+  Summary string `json:"summary,omitempty"`
 }
